652171746df2aa16ea76ba3a88b9c450098d0ab8
Only-Gaia <spinello.gaia@scuolaninomartoglio.edu.it>
2026-09-21 16:58:17 +0200

    List files in the current directory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import asyncio
-import logging
-
+import os
+print("FILES IN COGS:", sorted(os.listdir(os.path.dirname(os.path.abspath(__file__)))))
 import discord
 from discord.ext import commands
 
